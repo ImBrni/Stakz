ac0914531da1e3e078df4aa9ad876b5408ad3ccc
ImBrni <gepkiraly@gmail.com>
2025-10-24 12:10:02 +0200

    Forgot to extend these classes

--- src/main/java/casino/games/roulette/RouletteController.java
+++ src/main/java/casino/games/roulette/RouletteController.java
@@ -1,11 +1,12 @@
 package casino.games.roulette;
 
+import casino.controller.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class RouletteController {
+public class RouletteController extends BaseController {
 
     @GetMapping("/games/roulette")
     public String roulette(Model m) {
--- src/main/java/casino/games/wheel/WheelController.java
+++ src/main/java/casino/games/wheel/WheelController.java
@@ -1,11 +1,12 @@
 package casino.games.wheel;
 
+import casino.controller.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class WheelController {
+public class WheelController extends BaseController {
 
     @GetMapping("/games/wheel")
     public String roulette(Model m) {
