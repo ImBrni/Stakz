05ffcede6a26a646ecc42c551d84de8a7135dbb5
ImBrni <gepkiraly@gmail.com>
2025-12-11 08:25:01 +0100

    Unremoved blackjack

---
+++ src/main/java/casino/controller/games/blackjack/BlackjackController.java
@@ -0,0 +1,47 @@
+package casino.controller.games.blackjack;
+
+import casino.controller.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class BlackjackController extends BaseController {
+
+    @Autowired
+    private BlackjackService blackjackService;
+
+    @GetMapping("games/blackjack")
+    public String root(Model m) {
+
+        m.addAttribute("filler", true);
+        m.addAttribute("blackjack", blackjackService.getEmptyGame());
+
+        return "games/blackjack";
+    }
+
+    @PostMapping("games/blackjack/tick")
+    public String tick(@ModelAttribute Blackjack blackjack, Model m) {
+
+        switch (blackjack.getPaction()) {
+            case "start":
+                m.addAttribute("blackjack", blackjackService.startGame(blackjack));
+                return "games/blackjack";
+
+            case "double":
+                m.addAttribute("blackjack", blackjackService.actionDouble(blackjack));
+                return "games/blackjack";
+
+            case "hit":
+                m.addAttribute("blackjack", blackjackService.actionHit(blackjack));
+                return "games/blackjack";
+
+            default: // stand
+                m.addAttribute("blackjack", blackjackService.actionStand(blackjack));
+                return "games/blackjack";
+        }
+    }
+}
